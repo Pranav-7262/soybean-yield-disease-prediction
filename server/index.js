9e5645c2775078e2871936c2f0ac81f6658ead45
Pranav-7262 <varpepranav8@gmail.com>
2026-03-29 01:00:57 +0530

    fea:implemented frontend

--- server/index.js
+++ server/index.js
@@ -2,11 +2,15 @@ import express from "express";
 import cors from "cors";
 import yieldRoutes from "./routes/yieldRoutes.js";
 const app = express();
-app.use(cors());
+
 app.use(express.json());
-app.get("/", (req, res) => {
-  res.send("<b>Node Backend Running </b>");
-});
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Your React/Vite port
+    methods: ["GET", "POST"],
+    credentials: true,
+  }),
+);
 
 app.use("/api/yield", yieldRoutes);
 const PORT = process.env.PORT || 5000;
